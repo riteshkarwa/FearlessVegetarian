1c28671a221ef4b4d001fd18d0275d0501435980
ritesh karwa <riteshkarwa@yahoo.com>
2023-02-19 11:50:36 -0800

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -9,17 +9,25 @@ class Home extends Component {
           <Image  className="bugs_img" src="https://i.pinimg.com/564x/0b/bb/49/0bbb4991cac283330a29c537711f0ac2.jpg" width="200" height="200"  alt="Wht's up doc bugs bunny"/>
           In this space, I will be sharing some easy, fresh, flavorful, and  healthly recipes. These are family recipes I love and use daily.
           Growing up in a Hindu family, being vegetarian comes naturally to me. Hopefully for those who are finding it difficult to become a Vegetarian, 
-          Fearless Vegetarian is your friend. Remember, healthy doesn’t have to equal boring. So let’s get healthier, let’s get creative and let’s get cooking. <br/><br/>
+          Fearless Vegetarian is your friend. Remember, healthy doesn’t have to equal boring. So let’s get healthier, let’s get creative and let’s get cooking. If you have 
+          any questions reach out to us on our <a href="https://www.facebook.com/profile.php?id=100090174171697" className="med_link" target="_blank" rel="noopener noreferrer">Facebook Page</a>.
+          I wish I had someone to reach out to, or talk to about when I started cooking if you find yourself in the same situation reach out to us.<br/>
         </p>
         <p>
           <h2>Why Vegetarian Diets Are Better</h2>
-          Vegetarian diets, tend to be high in fiber and low in fat and cholesterol. On average, <a href="https://pubmed.ncbi.nlm.nih.gov/11434797/" className="med_link" target="_blank" rel="noopener noreferrer">vegetarians live ten years longer than meat-eaters.</a> 
+          Vegetarian diets, tend to be high in fiber and low in fat and cholesterol. On average, <a href="https://pubmed.ncbi.nlm.nih.gov/11434797/" className="med_link" target="_blank" rel="noopener noreferrer">vegetarians live ten years longer than meat-eaters. </a>  
           Over 80% of all cancers, cardiovascular diseases, and other degenerative illnesses can be kept in check with a plant-based diet.
           Red meat and processed meats are known to cause inflammation so for people with auto immune diseases vegetarian diet can prove to be benifical.
         </p>
         <p>
           <h2>Helping The Environment</h2>
           Recent studies show that one third of all global emissions are created by the food industry and meat production is responsible for over 50 percent of this! 
+        </p>
+        <p>
+          <h2>Tastes Yummy The Next Day</h2>
+          Vegetarian espically Indian Food is excellent for meal prep. It tastes better the next day so you can make it in large quantity. The ingredients
+          for most of the meals that I will be sharing are super cheap campared to eating out in resturant. Also you can control what is added to your food 
+          espically the spiece level. Beware when you use salt and chilli powder with caution if you add too much of either of those there is no coming back. 
         </p>
         <p>
           <h2>Fears of Vegetarian Diet Debunked:</h2>
