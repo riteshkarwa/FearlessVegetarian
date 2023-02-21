e5ac2a7adfdcf3e068cdc39495db1571ef2a60a6
ritesh karwa <riteshkarwa@yahoo.com>
2023-02-21 09:33:07 -0800

    Add files via upload

--- src/Wheretobegin.js
+++ src/Wheretobegin.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
+import Image from "react-bootstrap/Image";
 
 class Wheretobegin extends Component {
     render() {
       return (
         <div>
-            <h1>Where To Begin With Cooking Delicious Indian Food</h1>
             <p>
-                If you are trying to become vegetarian then you will come across lot of Indian dishes but it can be challenging if it is your first 
-                time cooking Indian Food. I was lucky enough to have my Mom guide me but that could not be the case with some of you. Indian Grocery 
+                <h1>Types of Vegetarians</h1>
+                <Image src="veg.jpg" thumbnail rounded></Image>
+            </p>
+            
+            <h1>Where To Begin With Cooking Delicious Vegetarian (aka lacto vegetarian) Food</h1>
+            <p>
+                If you are trying to become vegetarian (aka lacto vegetarian) then you will come across lot of Indian dishes but it can be challenging if it is your first 
+                time cooking Indian Food. I was lucky enough to have my Mom dearest to guide me but that could not be the case with some or most of you. Indian Grocery 
                 store is the answer to all your question. All the weird sounding spices can be found in the Indian grocery store. Another thing that no one 
                 tells you is that you have to be very careful with spices, (tumeric and chili powder) too much of any of those is a straight up death sentence 
-                for the dish and you willhave to start over. Please don't give up after first few unsucessful attempts at making a dish it gets easy after you 
-                have cooked couple of dishes. The interent is flooded with lot of vegan and vegetarian dishes but Indian food is by far the best one as it very 
-                flavourful. The ingredients for Indian food is generally not available in grocery store and if they are they are expensive. I would love to helo you 
-                guys out if you are stuck, you can reach out to me with questions on Facebook.
+                for the dish and you will have to start over. If you accidentally add to much chilli powder yougurt can help out but it depend on the dish you are making. 
+                Please don't give up after first few unsucessful attempts at making a dish it gets easy after you have cooked couple of dishes. The interent is flooded with lot of vegan and vegetarian dishes but Indian food is by far the best one as it very 
+                flavourful. The ingredients for Indian food is generally not available in big chain grocery store and if they are they are expensive. I would love to help you overcome
+                all the obstacles if you are stuck, that is what inspired me to start this wwebsite. 
             </p>
         </div>
       );
