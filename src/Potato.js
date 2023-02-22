02803855903f12a125c1c921bfeb2e848018be4a
ritesh karwa <riteshkarwa@yahoo.com>
2023-02-22 12:13:16 -0600

    Add files via upload

--- src/Potato.js
+++ src/Potato.js
@@ -23,13 +23,13 @@ class Potato extends Component {
         <ol> 
             <li>Take some olive oil in a non stick pan and heat it up</li> 
             <li>Slice the potatoes as shown in the image on a cutting board</li>
-            <li>Then use a food chopper to cut the potatoes into to small sqaure pieces</li>
+            <li>Then use a food chopper to cut the potatoes into to small square pieces</li>
             <li>Add 1 teaspoon cumin powder </li> 
             <li>Add the potatoes in the pan</li> 
-            <li>Cook the pototoes for about 15 to 20 minutes it they get transparent. Contantly stir the potatoe so they do not stick to the bottom of the pan</li> 
-            <li>One the pototoes starts to turn transparent add fine chopped onion</li> 
-            <li>Add all the spieces ginger powder, onion powder, coriander powder, chili powder, tumeric powder, curry powder</li> 
-            <li>Nicelxy stir the potatoe so the spices are nicely mixed </li> 
+            <li>Cook the potatoes for about 15 to 20 minutes it they get transparent. Constantly stir the potatoes so they do not stick to the bottom of the pan</li> 
+            <li>One the potatoes starts to turn transparent add fine chopped onion</li> 
+            <li>Add all the spices ginger powder, onion powder, coriander powder, chili powder, tumeric powder, curry powder</li> 
+            <li>Nicely stir the potatoes so the spices are nicely mixed </li> 
             <li>Add salt as per taste</li> 
         </ol>
       </div>
