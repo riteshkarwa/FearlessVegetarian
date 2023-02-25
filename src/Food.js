b6382197cb0640ed2cded700345db24a75edd611
ritesh karwa <riteshkarwa@yahoo.com>
2023-02-25 08:08:58 -0800

    Add files via upload

---
+++ src/Food.js
@@ -0,0 +1,51 @@
+export const Food = [
+    {
+        "id": 1,
+        card_img : "potato.png",
+        card_title:"Anytime Potato Seasoned with Indian Spices",
+        card_text:"My favorite go to meal, can be made for breakfast lunch or dinner. Takes roughly 30 minutes to cook.",
+        card_href : "/potato"
+    },
+    {
+        "id": 2,
+        card_img : "sandwich.png",
+        card_title:"Loaded Vegetable Sandwich",
+        card_text:"Appetizing Vegetable Sandwich super easy to make and very filling. It's a popular Indian food from the city of Mumbai",
+        card_href : "/sandwich"
+    },
+    {
+        "id": 3,
+        card_img : "peanutsd.jpg",
+        card_title:"Peanut Butter Toast with Apple slices and Cinnamon Sugar",
+        card_text:"Delicious Peanut Butter Toast with Apple slices and Cinnamon sugar, super easy to make and very filling.",
+        card_href : "/peanutsd"
+    },
+    {
+        "id": 4,
+        card_img : "potatocapsicum.jpg",
+        card_title:"Potato Curry with Roasted Red Pepper Sauce",
+        card_text:"Potato Curry with Roasted Red Pepper Sauce, Seasoned with Indian Spices. Takes roughly 45 minutes to cook.",
+        card_href : "/potatocapsicum"
+    },
+    {
+        "id": 5,
+        card_img : "beetroot.jpg",
+        card_title:"Beetroot Curry with Coconut Milk Served with Rice",
+        card_text:"The Beetroot Curry with Coconut Milk is quick and easy to make for a weeknight dinner or a summertime party at home.",
+        card_href : "/beetroot"
+    },
+    {
+        "id": 6,
+        card_img : "avocado.jpg",
+        card_title:"Avocado Toast",
+        card_text:"Super easy and delicious Avocado Toast. Takes roughly 15 minutes to cook. Good breakfast option if you have a busy schedule",
+        card_href : "/avocado"
+    },
+    {
+        "id": 7,
+        card_img : "chipotle_1.jpg",
+        card_title:"Chipotle Bowl",
+        card_text:"Appetizing Chipotle Bowl easy to make for get together or party.",
+        card_href : "/chipotle"
+    }
+]
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,108 +1,37 @@
 import React, { Component } from "react";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
- 
-class Recipe extends Component {
-  render() {
-    return (
-        <Container>
-            <Row>
-                <Col>
-                    <Card style={{ width: '20rem' , margin:'10px'}}>
-                        <Card.Img variant="top" src="potato.png" />
-                        <Card.Body>
-                            <Card.Title>Anytime Potato Seasoned with Indian Spices</Card.Title>
-                            <Card.Text>
-                            My favorite go to meal, can be made for breakfast lunch or dinner. Takes roughly 30 minutes to cook.
-                            </Card.Text>
-                            <Button variant="primary" href="/potato">Anytime Potato Recipe</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card style={{ width: '20rem' , margin:'10px'}}>
-                        <Card.Img variant="top" src="sandwich.png" />
-                        <Card.Body>
-                            <Card.Title>Loaded Vegetable Sandwich</Card.Title>
-                            <Card.Text>
-                            Appetizing Vegetable Sandwich super easy to make and very filling. It's a popular Indian food from the city of Mumbai
-                            </Card.Text>
-                            <Button variant="primary" href="/sandwich">Vegetable Sandwich Recipe</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card style={{ width: '20rem' , margin:'10px'}}>
-                        <Card.Img variant="top" src="peanutsd.jpg" />
-                        <Card.Body>
-                            <Card.Title>Peanut Butter Toast with Apple slices and Cinnamon Sugar</Card.Title>
-                            <Card.Text>
-                            Delicious Peanut Butter Toast with Apple slices and Cinnamon sugar, super easy to make and very filling.
-                            </Card.Text>
-                            <Button variant="primary" href="/peanutsd">Peanut Butter Apple Toast Recipe</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <Card style={{ width: '20rem' , margin:'10px'}}>
-                        <Card.Img variant="top" src="potatocapsicum.jpg" />
-                        <Card.Body>
-                            <Card.Title>Potato Curry with Roasted Red Pepper Sauce</Card.Title>
-                            <Card.Text>
-                            Potato Curry with Roasted Red Pepper Sauce, Seasoned with Indian Spices. Takes roughly 45 minutes to cook.
-                            </Card.Text>
-                            <Button variant="primary" href="/potatocapsicum">Potato Curry with Red Pepper Recipe</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card style={{ width: '20rem' , margin:'10px'}}>
-                        <Card.Img variant="top" src="beetroot.jpg" />
-                        <Card.Body>
-                            <Card.Title>Beetroot Curry with Coconut Milk Served with Rice</Card.Title>
-                            <Card.Text>
-                            The Beetroot Curry with Coconut Milk is quick and easy to make for a weeknight dinner or a summertime party at home.
-                            </Card.Text>
-                            <Button variant="primary" href="/beetroot">Beetroot Curry Recipe</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card style={{ width: '20rem' , margin:'10px'}}>
-                        <Card.Img variant="top" src="avocado.jpg" />
-                        <Card.Body>
-                            <Card.Title>Avocado Toast</Card.Title>
-                            <Card.Text>
-                            Super easy and delicious Avocado Toast. Takes roughly 15 minutes to cook. Good breakfast option if you have a busy schedule
-                            </Card.Text>
-                            <Button variant="primary" href="/avocado">Avocado Toast Recipe</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <Card style={{ width: '20rem' , margin:'10px'}}>
-                        <Card.Img variant="top" src="chipotle_1.jpg" />
-                        <Card.Body>
-                            <Card.Title>Chipotle Bowl</Card.Title>
-                            <Card.Text>
-                            Appetizing Chipotle Bowl easy to make for get together or party.
-                            </Card.Text>
-                            <Button variant="primary" href="/chipotle">Chipotle Bowl Recipe</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
+import { Food } from "./Food";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import {useState, useEffect} from 'react';
 
-            </Row>
+
+export default function Search(){
+
+      const [query, setQuery] = useState("");    
+      return (
+        <Container className="p-3">
+            <input type="text" placeholder="Search By Recipe Title" className="search" onChange={(e) => setQuery(e.target.value.toLowerCase())} />
+            <div className="search_container">
+                {Food.filter((f) =>f.card_title.toLowerCase().includes(query)
+                ).map((fd) => (
+                    <div>
+                        
+                        <Card className= "card_search"style={{ width: '23.5rem' , margin:'20px'}}>
+                            <Card.Img variant="top" src={fd.card_img} />
+                            <Card.Body>
+                                <Card.Title>{fd.card_title}</Card.Title>
+                                <Card.Text>
+                                {fd.card_text}
+                                </Card.Text>
+                                <Button variant="primary" href={fd.card_href}>Anytime Potato Recipe</Button>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                ))
+                }
+            </div>
         </Container>
-    );
-  }
+      );
+
 }
- 
-export default Recipe;
