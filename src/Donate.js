cd228ee2232d6513a7a8c144ca541758409e0193
ritesh karwa <riteshkarwa@yahoo.com>
2023-03-02 15:04:31 -0800

    Add files via upload

--- src/Donate.js
+++ src/Donate.js
@@ -9,7 +9,7 @@ class Donate extends Component {
         Hello friends, I hope you are having a good time browsing the Fearless Vegetarian website. We are constantly working on the website,
         making it easy to use for our customers. If you wish to help Fearless Vegetarian, you can do so by donating any amount using the 
         "PayPal Me" Button. It is your love and support that helps the Fearless Vegetarian team make improvements to the website and keep it running.
-        If you have question please do not hesitate to connect with us on our <a href="https://www.facebook.com/profile.php?id=100090174171697" className="med_link" target="_blank" rel="noopener noreferrer">Facebook Page.</a><br/><br/>
+        If you have question please do not hesitate to connect with us on our <a href="https://www.facebook.com/fearlessvegetarian" className="med_link" target="_blank" rel="noopener noreferrer">Facebook Page.</a><br/><br/>
         <Button variant="dark" href="https://paypal.me/riteshkarwa">PayPal Me</Button>
         </p>
       </div>
