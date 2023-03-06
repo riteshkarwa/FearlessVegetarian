797f40cbf2c88c6ca2e15513ccc02fe5d30b8b97
ritesh karwa <riteshkarwa@yahoo.com>
2023-03-06 15:21:21 -0800

    Add files via upload

--- src/Beetroot.js
+++ src/Beetroot.js
@@ -19,17 +19,17 @@ class Beetroot extends Component {
         </ol>
         <div className="ingredients">Instruction</div>
         <ol> 
-            <li>To begin making coconut curry boil the beetroot in a pot till it gets tender</li> 
-            <li>Once the beetroot is tender slice it up into circle and use food chopper to cut it into small square piece</li>
-            <li>Take some olive oil in a non stick pan and heat it up</li>
-            <li>Add beetroot pieces to the non stick pan</li> 
+            <li>To begin making beetroot curry boil the beetroot in a pressure cooker or pot till it gets tender</li> 
+            <li>Once the beetroot is tender slice it vertically and use food chopper to chop each slice into small square piece</li>
+            <li>Take some olive oil in a non stick pan and heat it up.</li>
+            <li>Add beetroot pieces to the non stick pan.</li> 
             <li>Add chopped Jalapeno peppers, salt, tumeric powder, coriander powder and Cinnamon stick</li> 
             <li>Pour a can of coconut milk</li> 
-            <li>Stir the ingredients and cook it on medium flame for 10-15 min close the pan with a lid to speed up the cooking</li> 
+            <li>Stir the ingredients and cook it on medium flame for 10-15 min close the pan with a lid to speed up the cooking process.</li> 
             <li>Heat a teaspoon of oil in a separate small pan.</li> 
-            <li>Add the mustard seeds and Masoor dal. Allow it to crackle and roast until for couple of minutes </li> 
+            <li>Add the mustard seeds and White Urad dal found in Indian Grocery Store. Allow it to crackle and roast for couple of minutes </li> 
             <li>Add salt as per taste</li>
-            <li>Your curry is ready taste it to add any spices. You can eat it with rice or Garlic Naan(Indian Bread) found in Indian Grocery Store</li>
+            <li>Your curry is ready, taste it to adjust spice level and salt. You can eat it with rice or Garlic Naan(Indian Bread) found in Indian Grocery Store</li>
         </ol>
         <h2>Final Product</h2>
         <Image src="beetroot.jpg" thumbnail rounded fluid></Image>
