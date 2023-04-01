5e91206c808a641a16cfa00cfe5814a8324143d8
ritesh karwa <riteshkarwa@yahoo.com>
2023-03-31 18:43:54 -0700

    potato

--- src/Potatobhurji.js
+++ src/Potatobhurji.js
@@ -49,7 +49,7 @@ class Potatobhurji extends Component {
           <Col><span className="material-symbols-outlined">restaurant_menu</span> Yields: 1 Serving</Col>
         </Row>
         <Row>         
-          <FontAwesomeIcon icon={faClock} />  Cooking Time: 60 minutes
+          <Col><FontAwesomeIcon icon={faClock} />  Cooking Time: 60 minutes</Col>
         </Row> 
         <div className="ingredients">Ingredients</div>
         <ol> 
