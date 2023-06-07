1f3cbd0de36647e7b1f06ef983d90e29f53a6964
ritesh karwa <riteshkarwa@yahoo.com>
2023-06-07 10:26:46 -0700

    Add files via upload

--- src/Notfound.js
+++ src/Notfound.js
@@ -6,12 +6,12 @@ class Notfound extends Component {
     render() {
       return (
         <div>
-            <Container>
+            <Container fluid className="p-0">
                 <p>
                     <Image src="nature.jpg" thumbnail rounded></Image>
                     "Oops! Looks like you've taken a wrong turn on the digital highway. Don't worry, even the best explorers get lost sometimes. 
                     While we try to figure out what went astray, why not enjoy nature pic. Take a moment to relax and we'll get you back on track soon. 
-                    Thanks for your patience, and remember, detours can lead to unexpected discoveries!" 
+                    Thanks for your patience, and remember, detours can lead to unexpected discoveries! Please check the url you have entered."
                 </p>
             </Container>
         </div>
