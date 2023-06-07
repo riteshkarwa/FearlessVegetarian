dec5fdf7e3e1b789b659dbbbec5d189e98fb4645
ritesh karwa <riteshkarwa@yahoo.com>
2023-06-07 16:51:36 -0700

    Add files via upload

--- src/Notfound.js
+++ src/Notfound.js
@@ -10,8 +10,9 @@ class Notfound extends Component {
                 <p>
                     <Image src="nature.jpg" thumbnail rounded></Image>
                     "Oops! Looks like you've taken a wrong turn on the digital highway. Don't worry, even the best explorers get lost sometimes. 
-                    While we try to figure out what went astray, why not enjoy nature pic. Take a moment to relax and we'll get you back on track soon. 
-                    Thanks for your patience, and remember, detours can lead to unexpected discoveries! Please check the url you have entered."
+                    While we try to figure out what went astray, why not enjoy a nature pic. Take a moment to relax and we'll get you back on track soon. 
+                    Thanks for your patience, and remember, detours can lead to unexpected discoveries! Please check the url you have entered maybe there
+                    is a spelling error."
                 </p>
             </Container>
         </div>
