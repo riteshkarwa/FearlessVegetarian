99ccb1ea141c169a673fbacf19cc06b06803a28c
ritesh karwa <riteshkarwa@yahoo.com>
2023-06-07 08:51:43 -0700

    Add files via upload

--- src/Notfound.js
+++ src/Notfound.js
@@ -5,12 +5,14 @@ class Notfound extends Component {
     render() {
       return (
         <div>
-            <p>
-                <Image src="moocow.jpg" thumbnail rounded></Image>
-                "Oops! Looks like you've taken a wrong turn on the digital highway. Don't worry, even the best explorers get lost sometimes. 
-                While we try to figure out what went astray, why not enjoy a virtual cup of coffee or tea? Take a moment to relax and we'll get you back on track soon. 
-                Thanks for your patience, and remember, detours can lead to unexpected discoveries!" 
-            </p>
+            <Container>
+                <p>
+                    <Image src="nature.jpg" thumbnail rounded></Image>
+                    "Oops! Looks like you've taken a wrong turn on the digital highway. Don't worry, even the best explorers get lost sometimes. 
+                    While we try to figure out what went astray, why not enjoy nature pic. Take a moment to relax and we'll get you back on track soon. 
+                    Thanks for your patience, and remember, detours can lead to unexpected discoveries!" 
+                </p>
+            </Container>
         </div>
       );
     }
