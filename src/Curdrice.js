c502e6ea0bb062ff17daa75fa17e1216689caa72
ritesh karwa <riteshkarwa@yahoo.com>
2023-06-23 18:38:14 -0700

    Add files via upload

--- src/Curdrice.js
+++ src/Curdrice.js
@@ -47,10 +47,10 @@ class Curdrice extends Component {
               </ol>
               <div className="ingredients">Instruction</div>
               <ol>
-                <li>Add rice to a bowl and rinse it well a few times. Then pour 1½ cups water and ½ teaspoon salt. Keep it in a pressure cooker. 
+                <li>Add rice to a bowl and rinse it well a few times. Then pour 1.5 cups water and 0.5 teaspoon salt. Keep it in a pressure cooker. 
                     Pressure cook rice for 3 whistles until soft. You can also cook the rice directly in the cooker for 2 whistles.
                     When the pressure releases, remove the rice & mash it lightly. You can also fluff up & use if you do not like mushy rice. 
-                    If you prefer to use milk, refer my notes below. Cool it completely.</li>
+                    If you prefer to use milk, ad 1/4 cup hot milk. Cool it completely.</li>
                 <li>Add curd to cooled rice. Mix well until combined. If you have cooked the rice without salt, then add it now.</li> 
                 <li>Stir in coriander leaves, grated carrots and cucumbers to the curd rice. Taste the curd rice & add more salt if needed.</li> 
                 <li>Heat oil in a pan. Then add cumin & mustard. When they begin to splutter, add red chilli, chana dal, urad dal and cashews 
@@ -58,7 +58,7 @@ class Curdrice extends Component {
                 <li>Add ginger, green chili and curry leaves.</li> 
                 <li>When the curry leaves turn crisp, add hing and turn off the stove. If using bottle gourd, add it to the hot seasoning and stir.</li> 
                 <li>Pour the curd rice mixture into the pan and mix all the ingredients well</li> 
-                <li></li> 
+                <li>I prefer hot curd rice so I cook the mixture in the pan for 5 minutes till it gets hot.</li> 
               </ol>
               <h2>Final Product</h2>
               <Image src="curdrice.jpg" thumbnail rounded fluid></Image>
