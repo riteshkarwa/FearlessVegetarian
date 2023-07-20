c173fcb317394f2741bde87d0154c84969a6feae
ritesh karwa <riteshkarwa@yahoo.com>
2023-07-19 20:13:50 -0700

    Add files via upload

--- src/About.js
+++ src/About.js
@@ -5,16 +5,16 @@ class About extends Component {
       return (
         <div>
             <p>
-              Welcome to Fearless Vegetarian! <br />
+              Welcome to Fearless Vegetarian! <br />  <br />
 
               At Fearless Vegetarian, we are passionate advocates of the vegetarian lifestyle.
               Our mission is to inspire and empower individuals to embrace the power of plant-based eating, 
-              promoting a healthier, more compassionate, and sustainable way of living. <br />
+              promoting a healthier, more compassionate, and sustainable way of living. <br />  <br />
 
               Founded by a group of food enthusiasts and health-conscious individuals, Fearless Vegetarian 
               aims to be your go-to resource for all things related to vegetarianism. Whether 
               you're a seasoned vegetarian, a curious beginner, or simply looking to incorporate more 
-              plant-based meals into your diet, we've got you covered. <br />
+              plant-based meals into your diet, we've got you covered. <br />  <br />
               
               Our website offers a wealth of information, including an extensive collection of delicious and 
               nutritious vegetarian recipes. From quick and easy meals for busy weeknights to gourmet 
@@ -23,32 +23,32 @@ class About extends Component {
               But Fearless Vegetarian is more than just a recipe website. We are dedicated to promoting 
               the broader benefits of vegetarianism. From improved health and wellness to environmental 
               sustainability and ethical considerations, we believe that every vegetarian choice can make a 
-              positive impact on ourselves and the world around us. <br />
+              positive impact on ourselves and the world around us. <br />  <br />
               
               On our platform, you'll find valuable resources, insightful articles, and practical tips for 
               adopting and maintaining a vegetarian lifestyle. We understand that transitioning to a plant-based 
               diet may seem daunting at first, so we're here to provide guidance, support, and 
-              encouragement every step of the way. <br />
+              encouragement every step of the way. <br />  <br />
               
               At Fearless Vegetarian, we believe that vegetarianism is not about limitations; it's about 
               embracing a world of exciting flavors, textures, and culinary possibilities. Our team is 
               dedicated to curating content that celebrates the richness of plant-based ingredients and 
-              encourages creativity in the kitchen. <br />
+              encourages creativity in the kitchen. <br />  <br />
               
               We invite you to join our vibrant and inclusive community of like-minded individuals who are 
               committed to making a difference through their food choices. Let's embark on this journey 
               together, fearlessly exploring the joys of vegetarianism and discovering the immense 
               satisfaction that comes from nourishing our bodies and souls with wholesome, 
-              compassionate meals.  <br />
+              compassionate meals.  <br />  <br />
               
               For any inquiries or questions, feel free to reach out to us at fearlessvegetarian62@gmail.com. 
-              We would love to hear from you! <br />
+              We would love to hear from you! <br />  <br />
               
               Thank you for visiting Fearless Vegetarian. We hope our website becomes a trusted 
               companion in your vegetarian journey, and we look forward to inspiring and supporting you 
-              every step of the way. <br />
+              every step of the way. <br />  <br />
 
-              With gratitude and excitement,
+              With gratitude and excitement,  <br />
               The Fearless Vegetarian Team
             </p>
         </div>
