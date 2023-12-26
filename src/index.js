0e3ac73293ab286d0e9df945191bf8da0324f2f6
ritesh karwa <riteshkarwa@yahoo.com>
2023-12-26 14:23:49 -0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import Main from "./Main";
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Helmet } from 'react-helmet';
 
 
 
 ReactDOM.render((
   <BrowserRouter>
+    <Helmet>
+        <title>Fearless Vegetarian</title>
+        <meta name="description" content="Discover a world of bold flavors with our Fearless Vegetarian recipes. Elevate your meatless meals with innovative, delicious dishes that prove vegetarian cooking can be exciting, satisfying, and packed with fearless flavors. Explore our collection now and embark on a culinary journey that celebrates the vibrant and diverse world of vegetarian cuisine" />
+    </Helmet>
     <Main/>
     </BrowserRouter>
     ),
