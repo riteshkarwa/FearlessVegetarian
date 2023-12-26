9cb835589bcbcf2172fc7c6fea450ed71cbe5688
ritesh karwa <riteshkarwa@yahoo.com>
2023-12-26 15:59:53 -0600

    Add files via upload

--- src/Pavbhaji.js
+++ src/Pavbhaji.js
@@ -6,6 +6,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import MediaQuery from 'react-responsive';
 import YouTube from 'react-youtube';
+import { Helmet } from "react-helmet";
 
 
 class Pavbhaji extends Component {
@@ -14,71 +15,84 @@ class Pavbhaji extends Component {
             height: '800',
             width: '1000',
         };
-    
+
         const optsMobile = {
             height: '550',
             width: '325',
         };
-        
-      return (
-        <div>
-            <h1>Pav Bhaji is an iconic dish from Mumbai. This Pav Bhaji Recipe is going to make you fall in love with it.</h1>
-            <Row className="cooktimerow">
-              <Col><span className="material-symbols-outlined">restaurant_menu</span> Yields: 4 Servings</Col>
-            </Row>
-            <Row className="cooktimerow">
-                <Col><FontAwesomeIcon icon={faClock} />  Cooking Time: 60 minutes</Col>
-            </Row>
-            <div className="ingredients">Ingredients</div>
-            <ol> 
-                <li>2 cups potato</li>
-                <li>1 Whole Red Pepper chopped</li>
-                <li>1/2 cup carrots peeled and chopped</li>
-                <li>1.5 cups cauliflower </li>
-                <li>1 cup tomato</li>
-                <li>1 cup onion</li>
-                <li>1 teaspoon chilli powder</li>
-                <li>1/4 cup raw onion for finely chopped for eating with Pav Bhaji</li>
-                <li>1 whole green chilli, 1/2 if the green chilli is big in size or if you are using Jalapeno pepper</li>
-                <></>
-            </ol>
-            <div className="ingredients">Instruction</div>
-            <ol>
-                <li>Rinse, peel and cube 3 medium or 2 larger potatoes (about 300 grams). Mine were about 1/2 to 2 cups when measured.</li>
-                <li>Clean 1 cup cauliflower florets (optional). Rinse 1⁄2 to 3⁄4 cups green peas. If you want you can also use 1 medium carrot (Half cup chopped).</li>
-                <li>For making masala, chop 1 medium onion (3/4 to 1 cup chopped), 2 medium tomatoes (3/4 to 1 cup chopped), half bell pepper (1⁄2 cup chopped) and 1 green chilli.</li> 
-                <li>Also make 1 tablespoon ginger garlic paste. Keep all of these aside.</li> 
-                <li>Add potatoes, cauliflower, peas & carrots (optional) to a pressure cooker or a instant pot. Pour 1.5 cups water, it should be just enough to cover them partially.</li>
-                <li>Cook the vegetables for 12 minutes and instant release the pressure once the instant pot beeps.</li>
-                <li>After opening the instant pot. All the vegetable should be cooked. Mash them well.</li>
-                <li>Add 1 cup onion to a non stick pot.Still them till they turn translucent.</li>
-                <li>Soak chopped Red pepper in hot water for 10 minutes</li>
-                <li>Using a blender puree the red pepper along with 3 cloves of garlic and few pieces of chopped ginger</li>
-                <li>Add 1 whole chopped green chilli to the non stick pot along with red pepper puree. Fry until it smells good.</li>
-                <li>Add 1 teaspoon chilli powder, 1/4 teaspoon tumeric powder, 2 heaping tablespoon paav bhaji masala powder found in Indian  Grocery Store.</li>
-                <li>Mix well and fry for 3 minutes</li>
-                <li>Add the boiled & mashed vegetables to the non stick pot. Add 1/2 to 3/4 cups more water to bring it to a consistency.</li>
-                <li>Mix well and simmer for a while to bring out the flavor of masala. Taste test and add more salt if you want. You can also add more chili powder at this stage for brighter color. 
-                    Also for more flavor you may add more pav bhaji masala if you want.</li>
-                <li>Add 3 tablespoon Rani Fenugreek Leaves Dried (Kasoori Methi). When it reaches the desired consistency, add chopped coriander leaves.Turn off the gas burner. Slightly cool down and add some lemon juice before serving.</li>
-                <li>Slit the pav horizontally leaving one edge intact. On a low flame, heat 1 teaspoon
-                    butter on a tawa. Add little masala powder over it.</li>
-                <li>Place the pav on the butter and toast for 1 to 2 mins until slightly crisp. You can do the same on the other side as well by adding more butter on the pan.</li>
-                <li>Garnish with coriander leaves, lemon wedges and chopped onions. You can add more butter on the hot bhaji.</li>
-            </ol>
-            <h2>Final Product</h2>
-            <Image src="pavbhaji.jpg" thumbnail rounded fluid></Image>
+
+        return (
             <div>
-                <h2>Pav Bhaji Preparation Video</h2>
-                <MediaQuery maxWidth={767}>
-                    <YouTube videoId="XDmBE6aW3ZU" opts={optsMobile} />
-                </MediaQuery>
-                <MediaQuery minWidth={767}>
-                    <YouTube videoId="XDmBE6aW3ZU" opts={optsDesktop} />
-                </MediaQuery>
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>Savor the Street-Style Magic: Homemade Pav Bhaji for a Flavorful Feast!</title>
+                    <link rel="canonical" href="https://fearlessvegetarian.netlify.app/pavbhaji" />
+                    <meta
+                        name="description"
+                        content="Experience the irresistible flavors of street-style indulgence with our Homemade Pav Bhaji recipe. This step-by-step guide will help you recreate the iconic Indian street food at home, offering a flavorful and satisfying feast. Dive into a medley of spiced vegetables served with buttery pav (bread rolls), creating a culinary delight that captures the essence of Indian street food. Elevate your dining experience and treat your taste buds to the magic of homemade Pav Bhaji. It's more than a meal; it's a street-style celebration on your plate!"
+                    />
+                    <meta
+                        name="keywords"
+                        content="pav bhaji, street-style food, homemade pav bhaji, Indian street food, flavorful feast, spiced vegetables, buttery pav, iconic Indian recipes, street food celebration, vegetarian delights, Indian cuisine at home, culinary indulgence, homemade street food, savory and satisfying, Indian culinary magic"
+                    />
+                </Helmet>
+                <h1>Pav Bhaji is an iconic dish from Mumbai. This Pav Bhaji Recipe is going to make you fall in love with it.</h1>
+                <Row className="cooktimerow">
+                    <Col><span className="material-symbols-outlined">restaurant_menu</span> Yields: 4 Servings</Col>
+                </Row>
+                <Row className="cooktimerow">
+                    <Col><FontAwesomeIcon icon={faClock} />  Cooking Time: 60 minutes</Col>
+                </Row>
+                <div className="ingredients">Ingredients</div>
+                <ol>
+                    <li>2 cups potato</li>
+                    <li>1 Whole Red Pepper chopped</li>
+                    <li>1/2 cup carrots peeled and chopped</li>
+                    <li>1.5 cups cauliflower </li>
+                    <li>1 cup tomato</li>
+                    <li>1 cup onion</li>
+                    <li>1 teaspoon chilli powder</li>
+                    <li>1/4 cup raw onion for finely chopped for eating with Pav Bhaji</li>
+                    <li>1 whole green chilli, 1/2 if the green chilli is big in size or if you are using Jalapeno pepper</li>
+                    <></>
+                </ol>
+                <div className="ingredients">Instruction</div>
+                <ol>
+                    <li>Rinse, peel and cube 3 medium or 2 larger potatoes (about 300 grams). Mine were about 1/2 to 2 cups when measured.</li>
+                    <li>Clean 1 cup cauliflower florets (optional). Rinse 1⁄2 to 3⁄4 cups green peas. If you want you can also use 1 medium carrot (Half cup chopped).</li>
+                    <li>For making masala, chop 1 medium onion (3/4 to 1 cup chopped), 2 medium tomatoes (3/4 to 1 cup chopped), half bell pepper (1⁄2 cup chopped) and 1 green chilli.</li>
+                    <li>Also make 1 tablespoon ginger garlic paste. Keep all of these aside.</li>
+                    <li>Add potatoes, cauliflower, peas & carrots (optional) to a pressure cooker or a instant pot. Pour 1.5 cups water, it should be just enough to cover them partially.</li>
+                    <li>Cook the vegetables for 12 minutes and instant release the pressure once the instant pot beeps.</li>
+                    <li>After opening the instant pot. All the vegetable should be cooked. Mash them well.</li>
+                    <li>Add 1 cup onion to a non stick pot.Still them till they turn translucent.</li>
+                    <li>Soak chopped Red pepper in hot water for 10 minutes</li>
+                    <li>Using a blender puree the red pepper along with 3 cloves of garlic and few pieces of chopped ginger</li>
+                    <li>Add 1 whole chopped green chilli to the non stick pot along with red pepper puree. Fry until it smells good.</li>
+                    <li>Add 1 teaspoon chilli powder, 1/4 teaspoon tumeric powder, 2 heaping tablespoon paav bhaji masala powder found in Indian  Grocery Store.</li>
+                    <li>Mix well and fry for 3 minutes</li>
+                    <li>Add the boiled & mashed vegetables to the non stick pot. Add 1/2 to 3/4 cups more water to bring it to a consistency.</li>
+                    <li>Mix well and simmer for a while to bring out the flavor of masala. Taste test and add more salt if you want. You can also add more chili powder at this stage for brighter color.
+                        Also for more flavor you may add more pav bhaji masala if you want.</li>
+                    <li>Add 3 tablespoon Rani Fenugreek Leaves Dried (Kasoori Methi). When it reaches the desired consistency, add chopped coriander leaves.Turn off the gas burner. Slightly cool down and add some lemon juice before serving.</li>
+                    <li>Slit the pav horizontally leaving one edge intact. On a low flame, heat 1 teaspoon
+                        butter on a tawa. Add little masala powder over it.</li>
+                    <li>Place the pav on the butter and toast for 1 to 2 mins until slightly crisp. You can do the same on the other side as well by adding more butter on the pan.</li>
+                    <li>Garnish with coriander leaves, lemon wedges and chopped onions. You can add more butter on the hot bhaji.</li>
+                </ol>
+                <h2>Final Product</h2>
+                <Image src="pavbhaji.jpg" thumbnail rounded fluid></Image>
+                <div>
+                    <h2>Pav Bhaji Preparation Video</h2>
+                    <MediaQuery maxWidth={767}>
+                        <YouTube videoId="XDmBE6aW3ZU" opts={optsMobile} />
+                    </MediaQuery>
+                    <MediaQuery minWidth={767}>
+                        <YouTube videoId="XDmBE6aW3ZU" opts={optsDesktop} />
+                    </MediaQuery>
+                </div>
             </div>
-        </div>
-      );
+        );
     }
 }
 
--- src/Poha.js
+++ src/Poha.js
@@ -6,75 +6,89 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import YouTube from 'react-youtube';
 import MediaQuery from 'react-responsive';
+import { Helmet } from "react-helmet";
 
 class Poha extends Component {
-    render() {
-      const optsDesktop = {
-        height: '800',
-        width: '1000',
-      };
-    
-      const optsMobile = {
-        height: '550',
-        width: '325',
-      };
-      return (
+  render() {
+    const optsDesktop = {
+      height: '800',
+      width: '1000',
+    };
+
+    const optsMobile = {
+      height: '550',
+      width: '325',
+    };
+    return (
+      <div>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Flavorful Poha Delight: A Quick and Wholesome Breakfast Recipe!</title>
+          <link rel="canonical" href="https://fearlessvegetarian.netlify.app/pavbhaji" />
+          <meta
+            name="description"
+            content="Start your day with our Flavorful Poha Delight, a quick and wholesome breakfast recipe that combines the goodness of flattened rice with aromatic spices and nutritious ingredients. This step-by-step guide ensures a delicious and satisfying morning meal that's easy to prepare. Elevate your breakfast routine with the flavors of Indian cuisine and enjoy a nutritious start to your day. Discover the joy of cooking with our Poha recipe – a perfect blend of simplicity and taste!"
+          />
+          <meta
+            name="keywords"
+            content="poha, breakfast recipe, Indian breakfast, quick and wholesome, flattened rice, nutritious morning meal, aromatic spices, easy breakfast, flavorful poha, Indian cuisine, breakfast delight, simple and tasty, morning comfort food, vegetarian breakfast, Indian culinary delight"
+          />
+        </Helmet>
+        <h1>The word Poha refers to 2 things – the first one is the ingredient flattened rice itself and the second one is the Poha recipe or the dish that is made with this particular ingredient</h1>
+        <Row className="cooktimerow">
+          <Col><span className="material-symbols-outlined">restaurant_menu</span> Yields: 2 Servings</Col>
+        </Row>
+        <Row className="cooktimerow">
+          <Col><FontAwesomeIcon icon={faClock} />  Cooking Time: 60 minutes</Col>
+        </Row>
+        <div className="ingredients">Ingredients</div>
+        <ol>
+          <li>1.5 cups (Poha) thick variety flattened rice easily found in Indian Grocery Store</li>
+          <li>1 teaspoon sugar or as required</li>
+          <li>1 teaspoon salt </li>
+          <li>1/4 teaspoon turmeric powder (ground turmeric)</li>
+          <li>2 tablespoons peanuts</li>
+          <li>1/2 teaspoon mustard seeds</li>
+          <li>1/2 teaspoon mustard seeds</li>
+          <li>1 whole potato big size or 2 potatoes small size chopped into square cubes</li>
+          <li>1 teaspoon green chilies – finely chopped or 1 to 2 green chillies</li>
+          <li>1 onion finely chopped</li>
+          <li>8 to 9 curry leaves (medium to large sized)</li>
+          <li>2 to 3 tablespoons coriander leaves – chopped (cilantro)</li>
+        </ol>
+        <div className="ingredients">Instruction</div>
+        <ol>
+          <li>Take 1.5 cups of thick or medium-thick Poha (thick variety flattened rice) first. Rinse them in clean running water in a strainer or colander thrice.
+            Rinsing in fresh clean water softens them as they absorb water. Make sure not to overdo the rinsing. This will make them mushy and pasty.</li>
+          <li>Dry roast 2 tablespoons peanuts in a heavy pan on medium-low heat until they become crunchy. Stir often for even cooking of the peanuts.
+            The peanuts should be roasted well and must not have any rawness in them. Roast till they become crunchy and have a few blisters on their skins.</li>
+          <li>Peel 1 medium to large size potato and chop into small cubes. In the same heavy non stick pan, heat 2 to 2.5 tablespoons olive oil.
+            Saute the potatoes in medium hot oil. Make sure to cut them in small cubes or else they will take more time to cook.</li>
+          <li>In the same pan, there will be some oil left. Add 1/2 teaspoon mustard seeds to the hot oil. If there is no oil, add 1 tablespoon olive oil and heat it gently.</li>
+          <li>When the mustard seeds splutter, add 1/2 teaspoon cumin seeds.</li>
+          <li>When the cumin seeds splutter, add ⅓ cup finely chopped onions. Sauté the onions until they soften and become translucent.</li>
+          <li>Now, add 8 to 9 curry leaves (medium to large size), 1 teaspoon chopped green chilies and the roasted peanuts. Mix well on low heat.</li>
+          <li>Add the Poha (thick variety flattened rice).</li>
+          <li>Mix gently with the rest of the ingredients.</li>
+          <li>Add the potatoes and mix very well. Cover the pan tightly with a lid and steam the Poha(thick variety flattened rice) for about 2 minutes on low heat. Remove the lid and check the taste. If required, add more salt and sugar.</li>
+          <li>Garnish with 2 to 3 tablespoons chopped coriander.</li>
+          <li>Mix gently.</li>
+          <li>Your Poha (thick variety flattened rice) is now ready. Squeeze some lemon juice before eating your Poha (thick variety flattened rice).</li>
+        </ol>
+        <h2>Final Product</h2>
+        <Image src="poha.jpg" thumbnail rounded fluid></Image>
         <div>
-            <h1>The word Poha refers to 2 things – the first one is the ingredient flattened rice itself and the second one is the Poha recipe or the dish that is made with this particular ingredient</h1>
-            <Row className="cooktimerow">
-              <Col><span className="material-symbols-outlined">restaurant_menu</span> Yields: 2 Servings</Col>
-            </Row>
-            <Row className="cooktimerow">
-                <Col><FontAwesomeIcon icon={faClock} />  Cooking Time: 60 minutes</Col>
-            </Row>
-            <div className="ingredients">Ingredients</div>
-            <ol> 
-                <li>1.5 cups (Poha) thick variety flattened rice easily found in Indian Grocery Store</li> 
-                <li>1 teaspoon sugar or as required</li> 
-                <li>1 teaspoon salt </li> 
-                <li>1/4 teaspoon turmeric powder (ground turmeric)</li> 
-                <li>2 tablespoons peanuts</li> 
-                <li>1/2 teaspoon mustard seeds</li> 
-                <li>1/2 teaspoon mustard seeds</li> 
-                <li>1 whole potato big size or 2 potatoes small size chopped into square cubes</li>
-                <li>1 teaspoon green chilies – finely chopped or 1 to 2 green chillies</li> 
-                <li>1 onion finely chopped</li> 
-                <li>8 to 9 curry leaves (medium to large sized)</li>
-                <li>2 to 3 tablespoons coriander leaves – chopped (cilantro)</li>
-            </ol>
-            <div className="ingredients">Instruction</div>
-            <ol> 
-                <li>Take 1.5 cups of thick or medium-thick Poha (thick variety flattened rice) first. Rinse them in clean running water in a strainer or colander thrice.
-                    Rinsing in fresh clean water softens them as they absorb water. Make sure not to overdo the rinsing. This will make them mushy and pasty.</li>
-                <li>Dry roast 2 tablespoons peanuts in a heavy pan on medium-low heat until they become crunchy. Stir often for even cooking of the peanuts.
-                    The peanuts should be roasted well and must not have any rawness in them. Roast till they become crunchy and have a few blisters on their skins.</li>
-                <li>Peel 1 medium to large size potato and chop into small cubes. In the same heavy non stick pan, heat 2 to 2.5 tablespoons olive oil. 
-                    Saute the potatoes in medium hot oil. Make sure to cut them in small cubes or else they will take more time to cook.</li>
-                <li>In the same pan, there will be some oil left. Add 1/2 teaspoon mustard seeds to the hot oil. If there is no oil, add 1 tablespoon olive oil and heat it gently.</li>
-                <li>When the mustard seeds splutter, add 1/2 teaspoon cumin seeds.</li>
-                <li>When the cumin seeds splutter, add ⅓ cup finely chopped onions. Sauté the onions until they soften and become translucent.</li> 
-                <li>Now, add 8 to 9 curry leaves (medium to large size), 1 teaspoon chopped green chilies and the roasted peanuts. Mix well on low heat.</li>
-                <li>Add the Poha (thick variety flattened rice).</li>
-                <li>Mix gently with the rest of the ingredients.</li>
-                <li>Add the potatoes and mix very well. Cover the pan tightly with a lid and steam the Poha(thick variety flattened rice) for about 2 minutes on low heat. Remove the lid and check the taste. If required, add more salt and sugar.</li>
-                <li>Garnish with 2 to 3 tablespoons chopped coriander.</li>
-                <li>Mix gently.</li> 
-                <li>Your Poha (thick variety flattened rice) is now ready. Squeeze some lemon juice before eating your Poha (thick variety flattened rice).</li>
-            </ol>
-            <h2>Final Product</h2>
-            <Image src="poha.jpg" thumbnail rounded fluid></Image>
-            <div>
-              <h2>Yogurt Preparation Video</h2>
-              <MediaQuery maxWidth={767}>
-                  <YouTube videoId="2FO9-kIQs20" opts={optsMobile} />
-              </MediaQuery>
-              <MediaQuery minWidth={767}>
-                  <YouTube videoId="2FO9-kIQs20" opts={optsDesktop} />
-              </MediaQuery>
-            </div>
+          <h2>Yogurt Preparation Video</h2>
+          <MediaQuery maxWidth={767}>
+            <YouTube videoId="2FO9-kIQs20" opts={optsMobile} />
+          </MediaQuery>
+          <MediaQuery minWidth={767}>
+            <YouTube videoId="2FO9-kIQs20" opts={optsDesktop} />
+          </MediaQuery>
         </div>
-      );
-    }
+      </div>
+    );
+  }
 }
-     
+
 export default Poha
