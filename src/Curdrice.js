29ce79138c3123096379113e44cfa9b443c8861f
ritesh karwa <riteshkarwa@yahoo.com>
2023-12-27 09:26:53 -0600

    Add files via upload

--- src/Curdrice.js
+++ src/Curdrice.js
@@ -6,6 +6,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import MediaQuery from 'react-responsive';
 import YouTube from 'react-youtube';
+import { Helmet } from "react-helmet";
 
 
 class Curdrice extends Component {
@@ -14,63 +15,76 @@ class Curdrice extends Component {
             height: '800',
             width: '1000',
         };
-      
+
         const optsMobile = {
             height: '550',
             width: '325',
         };
         return (
-          <div>
-              <h1>Curd rice is a South Indian dish made with precooked rice, Curd (Indian Yogurt), herbs and tempering spices. 
-                It is more often eaten by South Indians as a part of the meal, all round the year. Apart from being gut healthy, 
-                Curd rice is also a comforting summer food which keeps the body cool.</h1>
-              <Row className="cooktimerow">
-                <Col><span className="material-symbols-outlined">restaurant_menu</span> Yields: 1 Serving</Col>
-              </Row>
-              <Row className="cooktimerow">
-                  <Col><FontAwesomeIcon icon={faClock} />  Cooking Time: 30 minutes</Col>
-              </Row>
-              <div className="ingredients">Ingredients</div>
-              <ol> 
-                  <li>1/2 cup rice</li> 
-                  <li>1 green chilies – chopped finely </li> 
-                  <li>1 teaspoon oil</li> 
-                  <li>1/2 teaspoon mustard seeds</li> 
-                  <li>1/2 teaspoon cumin seeds</li> 
-                  <li>1 teaspoon chana dal found in Indian Grocery Store</li>
-                  <li>1 teaspoon Urad dal found in Indian Grocery Store</li> 
-                  <li>1/4 teaspoon hing (asafoetida) </li>
-                  <li>1/2 teaspoon ginger minced</li> 
-                  <li>8 cashews roasted</li> 
-                  <li>1 cup plain yogurt</li>
-              </ol>
-              <div className="ingredients">Instruction</div>
-              <ol>
-                <li>Add rice to a bowl and rinse it well a few times. Then pour 1.5 cups water and 0.5 teaspoon salt. Keep it in a pressure cooker. 
-                    Pressure cook rice for 3 whistles until soft. You can also cook the rice directly in the cooker for 2 whistles.
-                    When the pressure releases, remove the rice & mash it lightly. You can also fluff up & use if you do not like mushy rice. 
-                    If you prefer to use milk, ad 1/4 cup hot milk. Cool it completely.</li>
-                <li>Add curd to cooled rice. Mix well until combined. If you have cooked the rice without salt, then add it now.</li> 
-                <li>Stir in coriander leaves, grated carrots and cucumbers to the curd rice. Taste the curd rice & add more salt if needed.</li> 
-                <li>Heat oil in a pan. Then add cumin & mustard. When they begin to splutter, add red chilli, chana dal, urad dal and cashews 
-                    if using. Fry until the dal turns golden.</li> 
-                <li>Add ginger, green chili and curry leaves.</li> 
-                <li>When the curry leaves turn crisp, add hing and turn off the stove. If using bottle gourd, add it to the hot seasoning and stir.</li> 
-                <li>Pour the curd rice mixture into the pan and mix all the ingredients well</li> 
-                <li>I prefer hot curd rice so I cook the mixture in the pan for 5 minutes till it gets hot.</li> 
-              </ol>
-              <h2>Final Product</h2>
-              <Image src="curdrice.jpg" thumbnail rounded fluid></Image>
-              <div>
-                <h2>Curd Rice Preparation Video</h2>
-                <MediaQuery maxWidth={767}>
-                    <YouTube videoId="Y_QJdaanMzg" opts={optsMobile} />
-                </MediaQuery>
-                <MediaQuery minWidth={767}>
-                    <YouTube videoId="Y_QJdaanMzg" opts={optsDesktop} />
-                </MediaQuery>
-              </div>
-          </div>
+            <div>
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>Curd Rice Delight: A South Indian Dish with Precooked Rice, Indian Yogurt, Herbs, and Tempering Spices</title>
+                    <link rel="canonical" href="https://fearlessvegetarian.netlify.app/curdrice" />
+                    <meta
+                        name="description"
+                        content="Savor the goodness of our Curd Rice Delight, a quintessential South Indian dish featuring precooked rice, Indian yogurt, fragrant herbs, and a tempering of aromatic spices. This meta description invites you to experience the harmonious blend of textures and flavors in this cooling and comforting dish. Explore the simplicity and taste of South Indian cuisine with our easy-to-follow recipe. Elevate your dining experience with a bowl of Curd Rice that brings together tradition, freshness, and a burst of delightful spices."
+                    />
+                    <meta
+                        name="keywords"
+                        content="curd rice, South Indian dish, Indian yogurt, precooked rice, herbs, tempering spices, South Indian cuisine, traditional recipes, comfort food, cooling dish, easy-to-follow recipe, flavorful curd rice, Indian culinary delight, vegetarian dishes, fresh and aromatic"
+                    />
+                </Helmet>
+                <h1>Curd rice is a South Indian dish made with precooked rice, Curd (Indian Yogurt), herbs and tempering spices.
+                    It is more often eaten by South Indians as a part of the meal, all round the year. Apart from being gut healthy,
+                    Curd rice is also a comforting summer food which keeps the body cool.</h1>
+                <Row className="cooktimerow">
+                    <Col><span className="material-symbols-outlined">restaurant_menu</span> Yields: 1 Serving</Col>
+                </Row>
+                <Row className="cooktimerow">
+                    <Col><FontAwesomeIcon icon={faClock} />  Cooking Time: 30 minutes</Col>
+                </Row>
+                <div className="ingredients">Ingredients</div>
+                <ol>
+                    <li>1/2 cup rice</li>
+                    <li>1 green chilies – chopped finely </li>
+                    <li>1 teaspoon oil</li>
+                    <li>1/2 teaspoon mustard seeds</li>
+                    <li>1/2 teaspoon cumin seeds</li>
+                    <li>1 teaspoon chana dal found in Indian Grocery Store</li>
+                    <li>1 teaspoon Urad dal found in Indian Grocery Store</li>
+                    <li>1/4 teaspoon hing (asafoetida) </li>
+                    <li>1/2 teaspoon ginger minced</li>
+                    <li>8 cashews roasted</li>
+                    <li>1 cup plain yogurt</li>
+                </ol>
+                <div className="ingredients">Instruction</div>
+                <ol>
+                    <li>Add rice to a bowl and rinse it well a few times. Then pour 1.5 cups water and 0.5 teaspoon salt. Keep it in a pressure cooker.
+                        Pressure cook rice for 3 whistles until soft. You can also cook the rice directly in the cooker for 2 whistles.
+                        When the pressure releases, remove the rice & mash it lightly. You can also fluff up & use if you do not like mushy rice.
+                        If you prefer to use milk, ad 1/4 cup hot milk. Cool it completely.</li>
+                    <li>Add curd to cooled rice. Mix well until combined. If you have cooked the rice without salt, then add it now.</li>
+                    <li>Stir in coriander leaves, grated carrots and cucumbers to the curd rice. Taste the curd rice & add more salt if needed.</li>
+                    <li>Heat oil in a pan. Then add cumin & mustard. When they begin to splutter, add red chilli, chana dal, urad dal and cashews
+                        if using. Fry until the dal turns golden.</li>
+                    <li>Add ginger, green chili and curry leaves.</li>
+                    <li>When the curry leaves turn crisp, add hing and turn off the stove. If using bottle gourd, add it to the hot seasoning and stir.</li>
+                    <li>Pour the curd rice mixture into the pan and mix all the ingredients well</li>
+                    <li>I prefer hot curd rice so I cook the mixture in the pan for 5 minutes till it gets hot.</li>
+                </ol>
+                <h2>Final Product</h2>
+                <Image src="curdrice.jpg" thumbnail rounded fluid></Image>
+                <div>
+                    <h2>Curd Rice Preparation Video</h2>
+                    <MediaQuery maxWidth={767}>
+                        <YouTube videoId="Y_QJdaanMzg" opts={optsMobile} />
+                    </MediaQuery>
+                    <MediaQuery minWidth={767}>
+                        <YouTube videoId="Y_QJdaanMzg" opts={optsDesktop} />
+                    </MediaQuery>
+                </div>
+            </div>
         );
     }
 
