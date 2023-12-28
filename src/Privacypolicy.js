aa172daf56c8981a2dcc7a9ed77123cb2bc243d9
ritesh karwa <riteshkarwa@yahoo.com>
2023-12-27 20:39:46 -0600

    Add files via upload

--- src/Privacypolicy.js
+++ src/Privacypolicy.js
@@ -68,13 +68,15 @@ class Privacypolicy extends Component {
                 <h2>Types of Data Collected</h2>
                 <h3>Personal Data</h3>
                 <p>While using Our Service, We may ask You to provide Us with certain personally identifiable information that can be used to contact or identify You. Personally identifiable information may include, but is not limited to: </p>
-                <li>
-                    <h3><strong>Usage Data</strong></h3>
-                    <p>Usage Data is collected automatically when using the Service.</p>
-                    <p>Usage Data may include information such as Your Device's Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that You visit, the time and date of Your visit, the time spent on those pages, unique device identifiers and other diagnostic data.</p>
-                    <p>When You access the Service by or through a mobile device, We may collect certain information automatically, including, but not limited to, the type of mobile device You use, Your mobile device unique ID, the IP address of Your mobile device, Your mobile operating system, the type of mobile Internet browser You use, unique device identifiers and other diagnostic data.</p>
-                    <p>We may also collect information that Your browser sends whenever You visit our Service or when You access the Service by or through a mobile device.</p>
-                </li>
+                <ul>
+                    <li>
+                        <h3><strong>Usage Data</strong></h3>
+                        <p>Usage Data is collected automatically when using the Service.</p>
+                        <p>Usage Data may include information such as Your Device's Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that You visit, the time and date of Your visit, the time spent on those pages, unique device identifiers and other diagnostic data.</p>
+                        <p>When You access the Service by or through a mobile device, We may collect certain information automatically, including, but not limited to, the type of mobile device You use, Your mobile device unique ID, the IP address of Your mobile device, Your mobile operating system, the type of mobile Internet browser You use, unique device identifiers and other diagnostic data.</p>
+                        <p>We may also collect information that Your browser sends whenever You visit our Service or when You access the Service by or through a mobile device.</p>
+                    </li>
+                </ul>
                 <h3>Tracking Technologies and Cookies</h3>
                 <p>We use Cookies and similar tracking technologies to track the activity on Our Service and store certain information. Tracking technologies used are beacons, tags, and scripts to collect and track information and to improve and analyze Our Service. The technologies We use may include:</p>
                 <ul>
