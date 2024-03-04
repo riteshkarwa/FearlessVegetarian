6a5da215c42e73c60bb64ea42da1779fbce1e84d
ritesh karwa <riteshkarwa@yahoo.com>
2024-03-04 08:47:15 -0600

    Add files via upload

--- src/Bombaysandwich.js
+++ src/Bombaysandwich.js
@@ -47,36 +47,41 @@ class Bombaysandwich extends Component {
           <Col><FontAwesomeIcon icon={faClock} />  Cooking Time: 30 minutes</Col>
         </Row>
         <div className="ingredients">Ingredients</div>
-        <ol> 
-          <li>3 slices of sourdough bread or regular white bread</li> 
-          <li>1 tablespoon butter, per sandwich </li> 
-          <li>2 tablespoon of mint chutney </li> 
-          <li>2 russet potatoes, boiled and thinly sliced</li> 
-          <li>1 whole cucumber, thinly sliced</li> 
-          <li>2 plum tomatoes, thinly sliced</li> 
-          <li>Chat Masala found in Indian Grocery Store</li> 
+        <ol>
+          <li>3 slices of sourdough bread or regular white bread</li>
+          <li>1 tablespoon butter, per sandwich </li>
+          <li>2 tablespoon of mint chutney </li>
+          <li>2 russet potatoes, boiled and thinly sliced</li>
+          <li>1 whole cucumber, thinly sliced</li>
+          <li>2 plum tomatoes, thinly sliced</li>
+          <li>Chat Masala found in Indian Grocery Store</li>
           <li>1 whole potatoes big ones</li>
-          <li>Pinch of salt</li> 
-          <li>3 slices of Onions</li> 
+          <li>Pinch of salt</li>
+          <li>3 slices of Onions</li>
         </ol>
         <div className="ingredients">Instruction</div>
-        <ol> 
+        <ol>
           <li>The trick to boiling potatoes is to peel and slice them vertically and then boil it in water for 10 minutes.</li>
           <li>All the ingredients chopped up and ready to be layered on piece of bread.</li>
-          <li>Take 2 slices of white bread and toast it in a toaster. Spread a generous amount of butter on each slice of bread.</li> 
+          <li>Take 2 slices of white bread and toast it in a toaster. Spread a generous amount of butter on each slice of bread.</li>
           <li>Top with a layer of chutney.</li>
           <li>Top one slice of bread with potatoes followed by Chat Masala.</li>
-          <li>Add a layer of tomatoes on top of the potatoes and sprinkle some Chat Masala and salt.</li> 
-          <li>Add a layer of beetroot on top of the potatoes and sprinkle some Chat Masala and salt.</li> 
-          <li>Add a slice of muenster cheese.</li> 
-          <li>Add a piece of bread with layer of butter applied to the bottom of the bread.</li> 
-          <li>Add Mint Chutney to the top of the bread.</li> 
-          <li>Add a layer of onions on top of the bread and sprinkle some Chat Masala and salt.</li> 
-          <li>Add a layer of cucumber on top of the onions and sprinkle some Chat Masala and salt.</li> 
-          <li>Add a piece of bread with layer of butter applied to the bottom of the bread to complete our sandwich.</li>  
+          <li>Add a layer of tomatoes on top of the potatoes and sprinkle some Chat Masala and salt.</li>
+          <li>Add a layer of beetroot on top of the potatoes and sprinkle some Chat Masala and salt.</li>
+          <li>Add a slice of muenster cheese.</li>
+          <li>Add a piece of bread with layer of butter applied to the bottom of the bread.</li>
+          <li>Add Mint Chutney to the top of the bread.</li>
+          <li>Add a layer of onions on top of the bread and sprinkle some Chat Masala and salt.</li>
+          <li>Add a layer of cucumber on top of the onions and sprinkle some Chat Masala and salt.</li>
+          <li>Add a piece of bread with layer of butter applied to the bottom of the bread to complete our sandwich.</li>
         </ol>
         <h2>Final Product</h2>
-        <Image src="sandwich.jpg" thumbnail rounded fluid></Image>
+        <MediaQuery maxWidth={767}>
+          <Image src="sandwich.jpg" thumbnail rounded fluid></Image>
+        </MediaQuery>
+        <MediaQuery minWidth={767}>
+          <Image src="sandwich.jpg" thumbnail rounded width="50%" height="50%"></Image>
+        </MediaQuery>
         <div>
           <h2>Bombay Sandwich Video</h2>
           <MediaQuery maxWidth={767}>
@@ -90,5 +95,5 @@ class Bombaysandwich extends Component {
     );
   }
 }
- 
+
 export default Bombaysandwich;
