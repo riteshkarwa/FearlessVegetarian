34f7b2ac89079eb142cb03ad9e0216638a6c80a4
riteshkarwa <riteshkarwa@yahoo.com>
2024-10-16 16:34:28 -0500

    fix erayba page layout

--- src/Erayba.js
+++ src/Erayba.js
@@ -6,15 +6,23 @@ class Erayba extends Component {
             <div>
                 <p>
                     Erayba India! <br />  <br />
-                    Erayba Professional Hair Cosmetics – India Branch
+                    Erayba Professional Hair Cosmetics – India Branch <br /> 
 
-                    Erayba Professional Hair Cosmetics is excited to expand its global presence with the opening of our India branch. With the same commitment to excellence that has defined us since 1983, we aim to provide salons and professional hairdressers in India with top-tier hair cosmetics and cutting-edge solutions designed to empower stylists and colorists.
+                    Erayba Professional Hair Cosmetics is excited to expand its global presence with the opening of our India branch. 
+                    With the same commitment to excellence that has defined us since 1983, we aim to provide salons and professional hairdressers in India 
+                    with top-tier hair cosmetics and cutting-edge solutions designed to empower stylists and colorists. <br /> 
 
-                    Our India branch brings the latest in hair care technology and innovative products crafted from the best natural and vegan ingredients, ensuring that hair is treated with the utmost respect and care. We are committed to supporting the creativity and passion of India’s hair professionals, helping them achieve the best results and create true beauty.
+                    Our India branch brings the latest in hair care technology and innovative products crafted 
+                    from the best natural and vegan ingredients, ensuring that hair is treated with the utmost respect and care. 
+                    We are committed to supporting the creativity and passion of India’s hair professionals, 
+                    helping them achieve the best results and create true beauty. <br /> 
 
-                    A Commitment to Excellence and Sustainability
+                    <b>A Commitment to Excellence and Sustainability </b> <br /> 
 
-                    As part of our global mission, Erayba India emphasizes sustainability, blending high-quality hair cosmetics with eco-friendly practices to promote a healthy balance between beauty and nature. By supporting salons, stylists, and educators across India, we are confident in our ability to foster creativity and deliver the best results while respecting our environment.
+                    As part of our global mission, Erayba India emphasizes sustainability, 
+                    blending high-quality hair cosmetics with eco-friendly practices to promote a healthy balance between beauty and nature. 
+                    By supporting salons, stylists, and educators across India, we are confident in our ability to foster creativity 
+                    and deliver the best results while respecting our environment.
 
                     We are thrilled to bring Erayba's heritage and passion to India, ensuring beauty professionals here have access to world-class products and support.
 
