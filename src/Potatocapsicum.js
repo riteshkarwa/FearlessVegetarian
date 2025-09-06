b5f2f6f0fbe13407baa6d66a86b27c218d2f8e96
riteshkarwa <riteshkarwa@yahoo.com>
2025-09-06 17:50:06 -0500

    fix error

--- src/Potatocapsicum.js
+++ src/Potatocapsicum.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import Image from 'react-bootstrap/Image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock } from '@fortawesome/free-regular-svg-icons';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import YouTube from 'react-youtube';
 import MediaQuery from 'react-responsive';
 import Row from 'react-bootstrap/Row';
