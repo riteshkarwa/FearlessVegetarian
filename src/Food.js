3677a3ffc1dc0b8d6021edcc702898538140e23a
riteshkarwa <riteshkarwa@yahoo.com>
2026-06-07 09:31:41 -0500

    fix path couscousbalsamic

--- src/Food.js
+++ src/Food.js
@@ -391,6 +391,6 @@ export const Food = [
         card_title: "Middle Eastern Couscous Salad with Toasted Walnuts & Balsamic Vinaigrette",
         card_text: "Experience an explosion of rich, earthy flavors in every bite. This Middle Eastern-inspired couscous salad combines fluffy couscous, crunchy vegetables, fresh herbs, and toasted walnuts, all brought together with a tangy balsamic vinaigrette.",
         card_href: "/couscousbalsamic",
-        button_title: "Kesar Peda Recipe"
+        button_title: "Couscous Salad with Toasted Walnuts Recipe"
     }
 ]
