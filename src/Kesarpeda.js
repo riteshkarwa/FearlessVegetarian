827b3f1d1e1b22506a3536f0e904d5002c0c3900
riteshkarwa <riteshkarwa@yahoo.com>
2026-06-13 17:10:15 -0500

    remove duplicate import

--- src/Kesarpeda.js
+++ src/Kesarpeda.js
@@ -8,7 +8,6 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLeaf } from "@fortawesome/free-solid-svg-icons";
 import RecipeSchema from "./RecipeSchema";
 
